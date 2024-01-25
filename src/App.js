32c83ed4373ba8ea63e49e0978de99b9e25630ee
karthika M <karthikam0912@gmail.com>
2024-01-25 16:10:21 +0530

    Navbar

--- src/App.js
+++ src/App.js
@@ -5,10 +5,24 @@ import NavBar from './components/NavBar';
 //import FunctionalComponent from './components/FunctionalComponent';
 // import PropsState from './components/PropsState';
 // import State from './components/State';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import About from './components/About';
+import Work from './components/Work';
+import Contact from './components/Contact';
+import Home from './components/Home';
 function App() {
   return (
     <div className="App">
+     
+      <BrowserRouter>
       <NavBar/>
+      <Routes>
+        <Route path="/Home"element={<Home/>}></Route>
+        <Route path="/About"element={<About/>}></Route>
+        <Route path="/Work"element={<Work/>}></Route>
+        <Route path="/Contact"element={<Contact/>}></Route>
+      </Routes>
+      </BrowserRouter>
      {/* <PropsState name="karthika" age="20"/> */}
      {/* <State name="Karthika" college="KEC" dep="CSD" logo="https://www.kongu.edu/images/klogo.png"/> */}
     </div>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,15 +1,17 @@
+import React from "react";
+import { Link } from "react-router-dom";
 import "./style.css";
 
 const NavBar = () => {
   return (
     <ol>
-        <li>Home</li>
-        <li>About</li>
-        <li>Work</li>
-        <li>Achievements</li>
-        <li>Contact</li>
-        <li>Login</li>
+      <li><Link to="/Home">Home</Link></li>
+      <li><Link to="/About">About</Link></li>
+      <li><Link to="/Work">Work</Link></li>
+      <li><Link to="/Contact">Contact</Link></li>
+      <li><Link to="/Login">Login</Link></li>
     </ol>
   );
 };
+
 export default NavBar;
